1de28a2bd97069263099f7130311c255d4e57abb
Dustin Gault <Dustin.Gault@gmail.com>
2015-03-31 15:43:24 -0600

    fixed routes!?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331203714) do
+ActiveRecord::Schema.define(version: 20150331211443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 20150331203714) do
     t.string   "avatar"
     t.string   "credit_card"
     t.string   "billing_address"
+    t.integer  "status"
   end
 
 end
