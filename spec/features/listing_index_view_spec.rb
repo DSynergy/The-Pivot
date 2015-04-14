82e3f8414c3a03b7021801d643d71635392414ae
Kyra Weber <kyra.m.weber@gmail.com>
2015-04-14 11:28:24 -0600

    Implemented functionality to retire a host's listings

--- spec/features/listing_index_view_spec.rb
+++ spec/features/listing_index_view_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "listing index" do
   let(:listing) { Listing.create({title: "Bacon Maple Crunch",
-                                   description: "see title",
-                                   price: 8.00,
-                                   quantity_available: 2,
-                                   people_per_unit: 2,
-                                   private_bathroom: true,
-                                   start_date: "08/10/2015",
-                                   end_date: "08/12/2015",
-                                   user_id: 2,
-                                   country: 'USA',
-                                   state: 'Colorado',
-                                   city: 'Denver',
-                                   zipcode: '80206',
-                                   street_address: '1510 Blake St',
-                                   status: 0})}
+                                  description: "see title",
+                                  price: 8.00,
+                                  quantity_available: 2,
+                                  people_per_unit: 2,
+                                  private_bathroom: true,
+                                  start_date: "08/10/2015",
+                                  end_date: "08/12/2015",
+                                  user_id: 2,
+                                  country: 'USA',
+                                  state: 'Colorado',
+                                  city: 'Denver',
+                                  zipcode: '80206',
+                                  street_address: '1510 Blake St',
+                                  status: 0})}
 
   before(:each) do
     visit root_path
@@ -32,4 +32,16 @@ RSpec.describe "listing index" do
     expect(current_path).to eq listings_path
   end
 
+  context "Retired listings" do
+    it "do not show in index" do
+      listing1 = create(:listing, title: "home", start_date: "08/10/2015", end_date: "08/13/2015")
+      listing1.pictures.create(avatar: "default_image")
+      listing2 = create(:listing, title: "homey home", status: 1, start_date: "08/10/2015", end_date: "08/13/2015")
+      listing2.pictures.create(avatar: "default_image")
+
+      visit listings_path
+      expect(page).to have_content(listing1.title)
+      expect(page).not_to have_content(listing2.title)
+    end
+  end
 end
