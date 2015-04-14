523890b0d133acd5dcc295c063f318f68516e535
Dustin Gault <Dustin.Gault@gmail.com>
2015-04-13 19:40:33 -0600

    more emails

--- app/mailers/host_reservation_request_mailer.rb
+++ app/mailers/host_reservation_request_mailer.rb
@@ -1,10 +1,5 @@
 class HostReservationRequestMailer < ApplicationMailer
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.host_reservation_request_mailer.host_listing_email.subject
-  #
   def host_listing_email(reservation)
     @reservation = reservation
     @host = @reservation.listing.user
