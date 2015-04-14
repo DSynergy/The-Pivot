8e814020310d9e676c2ca10a073e6b292d913abc
Rich Shea <richcshea@gmail.com>
2015-04-13 18:13:19 -0600

    host can edit a listing

--- spec/features/host_functionality_feature_test.rb
+++ spec/features/host_functionality_feature_test.rb
@@ -31,7 +31,7 @@ RSpec.describe "host functionality" do
       sign_in(user)
       visit host_path(user)
       click_on("Add listing")
-      fill_in "listing[title]", with: "New Listing"
+      fill_in "listing[title]", with: "New Listing 2"
       fill_in "listing[description]", with: "New listing details"
       fill_in "listing[price]", with: 5000
       fill_in "listing[quantity_available]", with: 2
@@ -41,34 +41,34 @@ RSpec.describe "host functionality" do
       fill_in "listing[state]", with: "state"
       fill_in "listing[country]", with: "country"
       fill_in "listing[zipcode]", with: "12345"
-      fill_in "listing[start_date]", with: "05/10/15"
-      fill_in "listing[end_date]", with: "05/12/15"
+      fill_in "listing[start_date]", with: "06/10/15"
+      fill_in "listing[end_date]", with: "06/12/15"
       click_on("Submit New Listing")
       click_on("Edit")
       expect(current_path).to eq(edit_host_listing_path(slug: user.username, id: Listing.last.id))
       fill_in "listing[title]", with: "Updated Title"
+      fill_in "listing[start_date]", with: "06/10/15"
+      fill_in "listing[end_date]", with: "06/12/15"
       click_on("Update Changes")
       
       expect(page).to have_content("Updated Title")
     end
   end
+
+  def new_listing
+    click_on("Add listing")
+    fill_in "listing[title]", with: "New Listing"
+    fill_in "listing[description]", with: "New listing details"
+    fill_in "listing[price]", with: 5000
+    fill_in "listing[quantity_available]", with: 2
+    fill_in "listing[people_per_unit]", with: 4
+    fill_in "listing[street_address]", with: "123 new street"
+    fill_in "listing[city]", with: "city"
+    fill_in "listing[state]", with: "state"
+    fill_in "listing[country]", with: "country"
+    fill_in "listing[zipcode]", with: "12345"
+    fill_in "listing[start_date]", with: "05/10/15"
+    fill_in "listing[end_date]", with: "05/12/15"
+    click_on("Submit New Listing")
+  end
 end
-
-
-
-# 
-# A host must confirm a visitor's booking request
-#
-# As a host, I can edit the listing details.
-
-# As a host, I can retire a listing.
-#
-#   end
-# end
-#
-#
-#
-
-#As a user, when I reserve a listing, I receive an email notification with the details of my reservation.
-#As a user, when a host confirms the reservation, I receive an email notification.
-#As a host, when a reservation is requested, I receive an email notification.
