851f2d6a889de59f07360a60c180a600c2fd7a68
Kyra Weber <kyra.m.weber@gmail.com>
2015-04-14 12:03:08 -0600

    big feature test for retired listings

--- spec/features/listing_show_spec.rb
+++ spec/features/listing_show_spec.rb
@@ -26,25 +26,25 @@ RSpec.describe "Listing show" do
     expect(current_path).to eq(listings_path)
     click_on("Bacon Maple Crunch")
     expect(current_path).to eq(listing_path(listing))
-      expect(page).to have_content("Bacon Maple Crunch")
+    expect(page).to have_content("Bacon Maple Crunch")
   end
 
- it "displays listing description" do
+  it "displays listing description" do
     listing.pictures.create(avatar: 'default-image.jpg')
     visit listing_path(listing)
-      expect(page).to have_content("see title")
+    expect(page).to have_content("see title")
   end
 
- it "display listings price" do
+  it "display listings price" do
     listing.pictures.create(avatar: 'default-image.jpg')
     visit listing_path(listing)
     expect(page).to have_content("8.00")
   end
 
- it "displays a custom picture" do
+  it "displays a custom picture" do
     listing.pictures.create(avatar: "default_image.jpg")
     visit listing_path(listing)
-      expect(page).to have_css("img")
+    expect(page).to have_css("img")
   end
 
   it "has a link to add listing to cart", js: true do
@@ -61,17 +61,30 @@ RSpec.describe "Listing show" do
     expect(page).to have_selector("#flash_notice")
   end
 
-  context "when a user clicks an listing from a previous order thats been retired" do
-    xit "notes if listing is retired" do
+  context "when a user clicks an listing from a previous booking thats been retired" do
+    it "notes if listing is retired", js:true do
       user = create(:user)
-      user.bookings.create(status: 0, cart: { listing.id => ["08/10/2015", "08/10/2015"]})
+      listing.pictures.create(avatar: "default_image.jpg")
+      visit listing_path(listing)
 
-      login_as(user)
+      fill_in('listing[start_date]', with: "04/20/2015")
+      fill_in('listing[end_date]', with: "04/20/2015")
+
+      click_link_or_button("Add to Itinerary")
+      visit cart_path
+      click_link_or_button("Sign in to book itinerary")
+      fill_in("session[email_address]", with: "sadsal@example.com")
+      fill_in("session[password]", with: "password")
+      first(:css, "#small_submit_button").click
+      click_link_or_button("Book Itinerary")
+      a = page.driver.browser.switch_to.alert
+      a.accept
+
+      listing.update_attributes(status: 1)
+
       visit user_bookings_path(slug: user.username)
-      click_link_or_button("#{listing.id}")
-      within("#retired-listing") do
-        expect(page).to have_content("#{listing.title} has been retired from the menu")
-      end
+      click_link_or_button("#{listing.title}")
+      expect(page).to have_content("#{listing.title} has been retired from the listings")
       expect(page).not_to have_content("Add to Cart")
     end
   end
