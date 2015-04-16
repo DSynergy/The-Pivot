1410690a7af768ac3043d29614f670a6d143877f
Rich Shea <richcshea@gmail.com>
2015-04-15 18:02:38 -0600

    fix 3 of 4 booking spec tests

--- spec/features/user_booking_spec.rb
+++ spec/features/user_booking_spec.rb
@@ -10,24 +10,32 @@ RSpec.describe 'User confirm booking spec' do
     fill_in("session[password]", with: "password")
     first(:css, "#small_submit_button").click
   end
+  
+  let(:user2) {User.create(username: "Sally",
+                          email_address: "reallysadsal@example.com",
+                          password: "password",
+                          role: 0,
+                          credit_card: '1234-5678-9012-3456',
+                          billing_address: 'blahblahblah',
+                          display_name: 'SuperStarSally123')}
 
-  let!(:listing) { Listing.create({title: "Bacon Maple Crunch",
-                                   description: "see title",
-                                   price: 8.00,
-                                   quantity_available: 2,
-                                   people_per_unit: 2,
-                                   private_bathroom: true,
-                                   user_id: 2,
-                                   start_date: "08/10/2015",
-                                   end_date: "08/14/2015",
-                                   country: 'USA',
-                                   state: 'Colorado',
-                                   city: 'Denver',
-                                   zipcode: '80206',
-                                   street_address: '1510 Blake St',
-                                   status: 0})}
+  let!(:listing) {user2.listings.create(title: "Bacon Maple Crunch",
+                               description: "see title",
+                               price: 8.00,
+                               quantity_available: 2,
+                               people_per_unit: 2,
+                               private_bathroom: true,
+                               start_date: "08/07/2015",
+                               end_date: "08/10/2015",
+                               country: 'USA',
+                               state: 'Colorado',
+                               city: 'Denver',
+                               zipcode: '80206',
+                               street_address: '1510 Blake St',
+                               status: 0)}
 
-  let!(:listing2) { Listing.create({title: "Bacon",
+
+  let!(:listing2) { user2.listings.create({title: "Bacon",
                                     description: "see title",
                                     price: 10.00,
                                     quantity_available: 2,
@@ -35,13 +43,13 @@ RSpec.describe 'User confirm booking spec' do
                                     private_bathroom: true,
                                     start_date: "08/10/2015",
                                     end_date: "08/14/2015",
-                                    user_id: 2,
                                     country: 'USA',
                                     state: 'Colorado',
                                     city: 'Denver',
                                     zipcode: '80206',
                                     street_address: '1510 Blake St',
                                     status: 0})}
+
   def add_a_booking
     visit listing_path(listing)
     fill_in("listing[start_date]", with: "07/01/2015")
